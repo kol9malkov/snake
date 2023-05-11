5dc2873cf93b61fc65837482e1a54d7a8c303e42
kol9malkov <47697063+kol9malkov@users.noreply.github.com>
2023-05-11 20:54:03 +0300

    step1 to step2

--- step1.py
+++ step1.py
@@ -1,14 +1,31 @@
 import pygame, sys
+from pygame.math import Vector2
+
+class Fruit():
+    def __init__(self):
+        # позиции x и y
+        self.x = 5
+        self.y = 4
+        self.pos = Vector2(self.x, self.y)
+    
+    # отрисовка квадрата
+    def draw_fruit(self):
+        x_pos = int(self.pos.x * CELL_SIZE)
+        y_pos = int(self.pos.y * CELL_SIZE)
+        fruit_rect = pygame.Rect(x_pos, y_pos, CELL_SIZE, CELL_SIZE)
+        pygame.draw.rect(screen, RED, fruit_rect)
 
 """ Палитра цветов """
 JUNE_BUD = (175, 215, 70)
+RED = (255, 0, 0)
 """ Создания окна """
 pygame.init()
 CELL_SIZE = 40 # размер ячейки
 CELL_NUMBER = 20 # количество ячеек
 screen = pygame.display.set_mode((CELL_NUMBER * CELL_SIZE, CELL_NUMBER * CELL_SIZE))
 clock = pygame.time.Clock()
-
+""" Создание объектов """
+fruit = Fruit()
 """ Игровой цикл """
 while True:
     for event in pygame.event.get():
@@ -16,5 +33,7 @@ while True:
             pygame.quit()
             sys.exit()
     screen.fill(JUNE_BUD)
+    # отрисовка объектов
+    fruit.draw_fruit()
     pygame.display.update()
     clock.tick(60)
