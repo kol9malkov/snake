14d3ab13a6febda2bdd1846c0273af09fbab952d
kol9malkov <47697063+kol9malkov@users.noreply.github.com>
2023-05-11 20:57:20 +0300

    step7 to step8

--- step1.py
+++ step1.py
@@ -9,19 +9,20 @@ class Fruit():
     def draw_fruit(self):
         # создать прямоугольник и нарисовать его
         fruit_rect = pygame.Rect(int(self.pos.x * CELL_SIZE), int(self.pos.y * CELL_SIZE), CELL_SIZE, CELL_SIZE)
-        pygame.draw.rect(screen, RED, fruit_rect)
+        pygame.draw.rect(screen, PALE_GREEN, fruit_rect)
 
     def randomize(self):
         self.x = random.randint(0, CELL_NUMBER - 1)
         self.y = random.randint(0, CELL_NUMBER - 1)
         self.pos = Vector2(self.x, self.y)
 
+
 class Snake():
     def __init__(self):
-        self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+        self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
         self.direction = Vector2(1, 0)
         self.new_block = False
-    
+
     def draw_snake(self):
         for block in self.body:
             # создать прямоугольник и нарисовать его
@@ -41,7 +42,6 @@ class Snake():
             body_copy.insert(0, body_copy[0] + self.direction)
             self.body = body_copy[:]
 
-
     def add_block(self):
         self.new_block = True
 
@@ -49,10 +49,11 @@ class Main():
     def __init__(self):
         self.snake = Snake()
         self.fruit = Fruit()
-
+    
     def update(self):
         self.snake.move_snake()
         self.check_collision()
+        self.check_fail()
 
     def draw(self):
         self.fruit.draw_fruit()
@@ -60,15 +61,29 @@ class Main():
 
     def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
+            # обновить положение фрукта
             self.fruit.randomize()
+            # увеличить змею
             self.snake.add_block()
+    
+    def check_fail(self):
+        # змея за пределами экрана
+        if not 0 <= self.snake.body[0].x < CELL_NUMBER:
+            self.game_over()
+        if not 0 <= self.snake.body[0].y < CELL_NUMBER:
+            self.game_over()
+        # не касается саму себя
+        for block in self.snake.body[1:]:
+            if block == self.snake.body[0]:
+                self.game_over()
 
-    def add_block(self):
-        self.snake.add_block()
+    def game_over(self):
+        pygame.quit()
+        sys.exit()
 
 """ Палитра цветов """
 JUNE_BUD = (175, 215, 70)
-RED = (255, 0, 0)
+PALE_GREEN = (126, 166, 114)
 BLUE = (0, 0, 255)
 """ Создания окна """
 pygame.init()
@@ -76,7 +91,7 @@ CELL_SIZE = 40 # размер ячейки
 CELL_NUMBER = 20 # количество ячеек
 screen = pygame.display.set_mode((CELL_NUMBER * CELL_SIZE, CELL_NUMBER * CELL_SIZE))
 clock = pygame.time.Clock()
-""" Создание объектов """
+""" гланвый объект """
 main_game = Main()
 """ Таймер срабатывания движения """
 SCREEN_UPDATE = pygame.USEREVENT
@@ -91,13 +106,17 @@ while True:
             main_game.update()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                main_game.snake.direction = Vector2(0, -1)
+                if main_game.snake.direction.y != 1:
+                    main_game.snake.direction = Vector2(0, -1)
             if event.key == pygame.K_DOWN:
-                main_game.snake.direction = Vector2(0, 1)
+                if main_game.snake.direction.y != -1:
+                    main_game.snake.direction = Vector2(0, 1)
             if event.key == pygame.K_LEFT:
-                main_game.snake.direction = Vector2(-1, 0)
+                if main_game.snake.direction.x != 1:
+                    main_game.snake.direction = Vector2(-1, 0)
             if event.key == pygame.K_RIGHT:
-                main_game.snake.direction = Vector2(1, 0)
+                if main_game.snake.direction.x != -1:
+                    main_game.snake.direction = Vector2(1, 0)
 
     screen.fill(JUNE_BUD)
     main_game.draw()
